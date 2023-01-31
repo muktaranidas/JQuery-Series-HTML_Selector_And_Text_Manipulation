3fc62af08f1274b830b2b71c50dc594576fc1adf
Mukta-Rani-Das <mukta35-2733@diu.edu.bd>
2023-01-30 19:53:23 -0800

    first commit

---
+++ index.js
@@ -0,0 +1,7 @@
+// $(".my-div h1").text("Sudipta Dey");
+// $("#p1,h1").text("Mukta");
+// $("#p1").append("Mukta");
+// $("#p2").prepend("Mukta");
+
+var mypara1 = $("<p></p>").text("This is paragraph 0");
+$("#p2").after(mypara1);
